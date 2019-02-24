660f1691f2aba1293195df123b3f72661ac25e43
ZzzzzzzzzzzzzW <47723800+ZzzzzzzzzzzzzW@users.noreply.github.com>
2019-02-24 15:00:34 -0600

    print without title

--- products.py
+++ products.py
@@ -1,6 +1,16 @@
+#读取档案
+products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品, 价格' in line:
+			continue #继续
+		name, price = line.strip(). split(',')
+		products.append([name, price])
+
+print(products)
+
 #存数据
 
-products = []
 while True:
 	name = input('请输入商品名称：')
 	if name =='q':
