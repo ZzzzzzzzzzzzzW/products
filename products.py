f5cef7defbb4689d76641f11a25c4978f34fe851
ZzzzzzzzzzzzzW <47723800+ZzzzzzzzzzzzzW@users.noreply.github.com>
2019-04-19 12:41:05 -0500

    added function

--- products.py
+++ products.py
@@ -1,45 +1,52 @@
-#check file
 import os # operating system
-products = []
-if os.path.isfile('products.csv '):
-	print('yeah! 找到档案了！')
-	#读取档案
-	with open('products.csv', 'r', encoding='utf-8') as f:
-		for line in f:
-			if '商品, 价格' in line:
-				continue #继续
-			name, price = line.strip(). split(',')
-			products.append([name, price])
 
-	print(products)
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue # 繼續
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    return products
 
-else:
-	print('找不到档案.....')
+# 讓使用者輸入
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱: ')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格: ')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
 
-#存数据 
+# 印出所有購買紀錄
+def print_products(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
 
-while True:
-	name = input('请输入商品名称：')
-	if name =='q':
-		break
-	price = input('请输入商品价格：')
-	#p = []
-	#p.append(name)
-	#p.append(price)也可以看下面
+# 寫入檔案
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
 
-	#p = [name. price]也可以看下面
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): #检查档案在不在
+        print('yeah!找到档案了！')
+        products = read_file(filename)
+    else:
+        print('找不到档案')
 
-	products.append([name, price])
-#print(products)
 
 
-for p in products:
-	#print (p)
-	#print(p[0])就是映出商品名称
-	print(p[0], '的价格是', p[1])
-#  products[0][0] #取出数据
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
 
-with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('商品, 价格\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+
+main()
